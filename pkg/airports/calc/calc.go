dda0f9f2cead02ff664eaf29305666c984cc9918
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-04-25 12:33:40 -0400

    1 iteration; edge cases not handled

---
+++ pkg/airports/calc/calc.go
@@ -0,0 +1,87 @@
+package calc
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Metadata struct {
+	Origin, Destination int
+}
+
+func (m *Metadata) String() string {
+	return fmt.Sprintf("%d:%d", m.Origin, m.Destination)
+}
+
+var InvalidSequence = errors.New("invalid sequence")
+
+func ProcessFlights(flights [][]string) ([]string, error) {
+	var findOrig, findDest string
+	m := map[string]*Metadata{}
+
+	for i := range flights {
+
+		// let it panic if input is invalid
+		// TODO I didn't decide yet if I want to have duplicate here or only validation on JSON
+		origin := flights[i][0]
+		destin := flights[i][1]
+
+		if _, exist := m[origin]; !exist {
+			m[origin] = &Metadata{Origin: 1}
+		} else {
+			m[origin].Origin += 1
+		}
+		if _, exist := m[destin]; !exist {
+			m[destin] = &Metadata{Destination: 1}
+		} else {
+			m[destin].Destination += 1
+		}
+	}
+	fmt.Printf("%v\n", m)
+	for k, v := range m {
+		if (v.Origin+v.Destination)%2 == 1 {
+
+			if v.Origin%2 == 1 {
+				fmt.Println(k)
+				if findOrig != "" {
+					return nil, InvalidSequence
+				} else {
+					findOrig = k
+				}
+			}
+			if v.Destination%2 == 1 {
+				if findDest != "" {
+					return nil, InvalidSequence
+				} else {
+					findDest = k
+				}
+			}
+		}
+	}
+
+	return []string{findOrig, findDest}, nil
+
+}
+
+//type Node struct {
+//	Origin, Destination *Node
+//
+//	Name string
+//}
+//func ProcessFlights(flights [][]string) ([]string, error) {
+//	m := map[string]*Node{}
+//
+//	head := nil
+//	tail := nil
+//
+//	for i := range flights {
+//		origin := flights[i][0]
+//		destin := flights[i][1]
+//
+//		if val, ok := m[flights[i][0]]; !ok {
+//			m[flights[i][0]]
+//		}
+//	}
+//
+//	return nil, nil
+//}
---
+++ pkg/airports/calc/calc_test.go
@@ -0,0 +1,40 @@
+package calc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestProcessFlights(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    [][]string
+		expected []string
+	}{
+		{"Test Case 1", [][]string{{"SFO", "EWR"}}, []string{"SFO", "EWR"}},
+		{"Test Case 2", [][]string{{"ATL", "EWR"}, {"SFO", "ATL"}}, []string{"SFO", "EWR"}},
+		{"Test Case 3", [][]string{{"IND", "EWR"}, {"SFO", "ATL"}, {"GSO", "IND"}, {"ATL", "GSO"}}, []string{"SFO", "EWR"}},
+
+		// NO SOLUTION; sequence is invalid with GTO
+		//{"Test Case 4", [][]string{{"IND", "EWR"}, {"SFO", "ATL"}, {"SFO", "GTO"}, {"GSO", "IND"}, {"ATL", "GSO"}}, []string{"SFO", "EWR"}},
+
+		// Loop. Imagine we have need from Paris to SaoPaolo. And we have cheap flight from Paris to Berlin and from Berlin to SaoPaolo with stop in Paris
+		//{"Test Case 4", [][]string{{"SFO", "ATL"}, {"ATL", "SFO"}, {"SFO", "GRU"}}, []string{"SFO", "GRU"}},
+
+		// NO SOLUTION. It's infinite loop; We can't say where is origin and destination
+		//{"Test Case 4", [][]string{{"SFO", "ATL"}, {"ATL", "SFO"}}, []string{}},
+
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := ProcessFlights(tc.input)
+			if err != nil {
+				t.Fatalf("Error processing flights: %v", err)
+			}
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Fatalf("Expected: %v, but got: %v", tc.expected, result)
+			}
+		})
+	}
+}
